ba4afdbf69112821d0f7a1b77130b721eb41d79a
M.Umar Rathore <f190328@nu.edu.pk>
2021-12-11 07:15:57 +0500

    Frontend Styling

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const expressEjsLayout = require('express-ejs-layouts')
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require("passport");
+
 //passport config:
 require('./config/passport')(passport)
     //mongoose
@@ -29,13 +30,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-})
-
-//Routes
+        res.locals.success_msg = req.flash('success_msg');
+        res.locals.error_msg = req.flash('error_msg');
+        res.locals.error = req.flash('error');
+        next();
+    })
+    //Routes
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
